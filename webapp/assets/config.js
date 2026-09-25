96f1c7fa2206cb741d208ed6bcc208224dcef4ce
bhagirathinursinghome <bhagirathiacc00@gmail.com>
2026-09-25 07:16:18 +0530

    Update config.js

--- webapp/assets/config.js
+++ webapp/assets/config.js
@@ -4,3 +4,5 @@ window.BNH_CONFIG = {
   SUPABASE_KEY: "sb_publishable_tzf_Bhqi-QFGdVtYfKo9dw_QqwXD5hv",
   APP_NAME: "Bhagirathy Nursing Home"
 };
+window.BNH_CONFIG.WORK_LOCATION = { label: "Bhagirathi Diagnostic & Health Care", lat: 24.460804, lng: 88.073206 };
+window.BNH_CONFIG.RADIUS_METERS = 30;
